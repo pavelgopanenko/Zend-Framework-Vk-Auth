2175a36ddcc60e9a92b41777916c6755a860f0aa
pavelgopanenko <pavelgopanenko@gmail.com>
2011-05-28 18:34:09 +0700

    Authentitcation adapters and tests

--- README.markdown
+++ README.markdown
@@ -1,1 +1,5 @@
-In progress
+Stable version 1.0.0
+
+Development for [Zend Framework 1.11.* version](http://framework.zend.com/changelog/1.11.6), on earlyer 1.11 work not testing.
+
+description in progress...
--- Ulrika/Auth/Adapter/Vk/Abstract.php
+++ Ulrika/Auth/Adapter/Vk/Abstract.php
@@ -17,90 +17,90 @@
  */
 abstract class Ulrika_Auth_Adapter_Vk_Abstract implements Zend_Auth_Adapter_Interface
 {
-	private $_config;
+    private $_config;
 
-	/**
-	 * Return VK application identity.
-	 * @return string
-	 * @throws RuntimeException Application identity not found.
-	 */
-	protected function _getAppId()
-	{
-		if (!isset($this->_config['appId'])) {
-			throw new RuntimeException('Parameter "appId" not exists in config.');
-		}
+    /**
+     * Return VK application identity.
+     * @return string
+     * @throws RuntimeException Application identity not found.
+     */
+    protected function _getAppId()
+    {
+        if (!isset($this->_config['appId'])) {
+            throw new RuntimeException('Parameter "appId" not exists in config.');
+        }
 
-		return $this->_config['appId'];
-	}
+        return $this->_config['appId'];
+    }
 
-	/**
-	 * Return VK application secret.
-	 * @return string
-	 * @throws RuntimeException Application secret not found.
-	 */
-	protected function _getSecret()
-	{
-		if (!isset($this->_config['secret'])) {
-			throw new RuntimeException('Parameter "secret" not exists in config.');
-		}
+    /**
+     * Return VK application secret.
+     * @return string
+     * @throws RuntimeException Application secret not found.
+     */
+    protected function _getSecret()
+    {
+        if (!isset($this->_config['secret'])) {
+            throw new RuntimeException('Parameter "secret" not exists in config.');
+        }
 
-		return $this->_config['secret'];
-	}
+        return $this->_config['secret'];
+    }
 
 
-	private $_request;
+    private $_request;
 
-	/**
-	 * Return request instance.
-	 * @return Zend_Controller_Request_Abstract
-	 */
-	public function getRequest()
-	{
-		return $this->_request;
-	}
+    /**
+     * Return request instance.
+     * @return Zend_Controller_Request_Abstract
+     */
+    public function getRequest()
+    {
+        return $this->_request;
+    }
 
-	/**
-	 * Set request instance where contains auth parameters.
-	 * @param Zend_Controller_Request_Abstract $request
-	 */
-	public function setRequest(Zend_Controller_Request_Abstract $request)
-	{
-		$this->_request = $request;
-	}
+    /**
+     * Set request instance where contains auth parameters.
+     * @param Zend_Controller_Request_Abstract $request
+     */
+    public function setRequest(Zend_Controller_Request_Abstract $request)
+    {
+        $this->_request = $request;
+    }
 
-	/**
-	 * Contructor.
-	 * @param array|ArrayAccess|Zend_Config $config VK social network application configuration.
-	 * @throws \InvalidArgumentException Config type argument not support
-	 */
-	public function __construct($config = null)
-	{
-		if ($config instanceof Zend_Config) {
-			$config = $config->toArray();
-		} elseif (!is_array($config) && !($config instanceof ArrayAccess)) {
-			throw new \InvalidArgumentException('Config parameter must be array or Zend_Config instance.');
-		}
+    /**
+     * Contructor.
+     * @param array|ArrayAccess|Zend_Config $config VK social network application configuration.
+     * @throws InvalidArgumentException Config type argument not support
+     */
+    public function __construct($config = null)
+    {
+        if ($config instanceof Zend_Config) {
+            $config = $config->toArray();
+        } elseif (!is_array($config) && !($config instanceof ArrayAccess)) {
+            throw new \InvalidArgumentException('Config parameter must be array or Zend_Config instance.');
+        }
 
-		$this->_config = $config;
-	}
+        $this->_config = $config;
+    }
 
-	/**
-	 * (non-PHPdoc)
-	 * @see Zend_Auth_Adapter_Interface::authenticate()
-	 */
-	public function authenticate()
-	{
-		if (null === ($request = $this->getRequest())) {
-			throw new \LogicException('Not set http-request instance.');
-		}
+    /**
+     * (non-PHPdoc)
+     * @see Zend_Auth_Adapter_Interface::authenticate()
+     */
+    public function authenticate()
+    {
+        if (null === ($request = $this->getRequest())) {
+            throw new \LogicException('Not set http-request instance.');
+        }
 
-		return $this->_handleAuthRequest($request);
-	}
+        return $this->_handleAuthRequest($request);
+    }
 
-	/**
-	 * Vk http-request with auth params handler.
-	 * @param Zend_Controller_Request_Abstract $request
-	 * @return Zend_Auth_Result
-	 */
-	abstract protected function _handleAuthRequest(Zend_Controller_Request_Abstract $request);
+    /**
+     * Vk http-request with auth params handler.
+     * @param Zend_Controller_Request_Abstract $request
+     * @return Zend_Auth_Result
+     */
+    abstract protected function _handleAuthRequest(Zend_Controller_Request_Abstract $request);
 }
